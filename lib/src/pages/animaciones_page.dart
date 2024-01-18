ccac1267447656644f8ae0bd52c0e7876448f620
Carlos DIaz <c.diazadriann@gmail.com>
2024-01-18 17:12:10 -0400

    TErmine Animaciones

--- lib/src/pages/animaciones_page.dart
+++ lib/src/pages/animaciones_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 class AnimacionesPage extends StatelessWidget {
   const AnimacionesPage({super.key});
@@ -22,10 +23,72 @@ class CuadradoAnimado extends StatefulWidget {
   State<CuadradoAnimado> createState() => _CuadradoAnimadoState();
 }
 
-class _CuadradoAnimadoState extends State<CuadradoAnimado> {
+class _CuadradoAnimadoState extends State<CuadradoAnimado>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> rotacion;
+  late Animation<double> opacidad;
+  late Animation<double> opacidadOut;
+  late Animation<double> mover;
+  late Animation<double> agrandar;
+
+  @override
+  void initState() {
+    controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 4000));
+
+    rotacion = Tween(begin: 0.0, end: 4.0 * math.pi)
+        .animate(CurvedAnimation(parent: controller, curve: Curves.bounceIn));
+
+    opacidad = Tween(begin: 0.1, end: 1.0)
+        .animate(CurvedAnimation(parent: controller, curve: Interval(0, 0.25)));
+
+    opacidadOut = Tween(begin: 0.0, end: 1.0).animate(
+        CurvedAnimation(parent: controller, curve: Interval(0.75, 1.0)));
+
+    mover = Tween(begin: 0.0, end: 200.0).animate(controller);
+
+    agrandar = Tween(begin: 0.0, end: 2.0)
+        .animate(CurvedAnimation(parent: controller, curve: Curves.easeOut));
+
+    controller.addListener(() {
+      if (controller.status == AnimationStatus.completed) {
+        //controller.reverse();
+        //controller.reset();
+        controller.repeat();
+      }
+      //else if (controller.status == AnimationStatus.dismissed) {
+      //  controller.forward();
+      //}
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const _Rectangulo();
+    //Paly o reproducion;
+    controller.forward();
+    return AnimatedBuilder(
+      animation: controller,
+      child: const _Rectangulo(),
+      builder: (BuildContext context, Widget? child) {
+        return Transform.translate(
+          offset: Offset(mover.value, mover.value),
+          child: Transform.rotate(
+              angle: rotacion.value,
+              child: Opacity(
+                  opacity: opacidad.value - opacidadOut.value,
+                  child: Transform.scale(scale: agrandar.value, child: child))),
+        );
+      },
+    );
+    //return const _Rectangulo();
   }
 }
 
@@ -37,7 +100,7 @@ class _Rectangulo extends StatelessWidget {
     return Container(
       width: 70,
       height: 70,
-      decoration: const BoxDecoration(color: Colors.black),
+      decoration: const BoxDecoration(color: Colors.green),
     );
   }
 }
